268ccbba379fbe5e76580dfe4ebe63d5084def95
Andile Mchunu <andykneivel@gmail.com>
2023-05-17 15:08:08 +0200

    Initial Commit

---
+++ My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by DA MAC M1 161 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
